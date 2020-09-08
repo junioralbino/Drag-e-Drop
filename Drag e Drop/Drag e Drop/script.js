8f85a0359dfa0220529367bb58e2c3eb48d31e0a
junioralbino741@gmail.com <34866064+junioralbino@users.noreply.github.com>
2020-09-08 19:11:21 -0300

    Add files via upload

---
+++ Drag e Drop/Drag e Drop/script.js
@@ -0,0 +1,27 @@
+$(function(){
+
+  $('.objetos').bind('mousedown', function(){
+
+  	$('.objetos').bind('mousemove', function(e){
+  		var x = e.originalEvent.pageX;
+  		var y = e.originalEvent.pageY;
+
+  		var width = $(this).width();
+  		var height = $(this).height();
+
+  		$(this).css('left', (x - (width/2)) +'px');
+  		$(this).css('top',(y - (height/2)) +'px');
+
+  	});
+
+  });
+ 
+ $('.obejtos').bind('mouseup', function(){
+
+ 	$('.objetos').unbind('mousemove', function(){
+
+ 	});
+
+ });
+
+});
